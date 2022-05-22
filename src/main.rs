8db804260d40c8622a8900be432ca18fff18f6f1
sjquant <seonujang92@gmail.com>
2022-05-22 20:09:51 +0900

    🚧 Simply handle commands

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,50 @@ struct Cli {
 enum Commands {
     Start { name: String },
     Finish { name: String },
-    Release,
 }
 
 fn main() {
     let cli = Cli::parse();
     match &cli.command {
         Commands::Start { name } => {
-            println!("Start {}", name)
+            on_start(name);
         }
         Commands::Finish { name } => {
-            println!("Finish {}", name)
+            on_finish(name);
         }
-        Commands::Release => {
-            println!("Release")
+    }
+}
+
+fn on_start(name: &String) {
+    match name.to_lowercase().as_str() {
+        "feature" => {
+            println!("Create or Move to the feature branch");
+        }
+        "hotfix" => {
+            println!("Move to the latest tag");
+            println!("Cherry-pick changes from the feature branch (Suggest commits)");
+        }
+        _ => {
+            println!("feature and hotfix are only valid names");
+            std::process::exit(1);
+        }
+    }
+}
+
+fn on_finish(name: &String) {
+    match name.to_lowercase().as_str() {
+        "feature" => {
+            println!("Push branch to remote");
+            println!("Create a pull request to main branch if not already done");
+        }
+        "hotfix" => {
+            println!("Validate whether it's from tag, not feature branch");
+            println!("Tag the commit");
+            println!("Push the tag to the remote");
+        }
+        _ => {
+            println!("feature and hotfix are only valid names");
+            std::process::exit(1);
         }
     }
 }
