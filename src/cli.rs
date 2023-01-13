5e3b872a2186c0966d90faabf55a282da13a669f
sjquant <seonujang92@gmail.com>
2023-01-13 12:45:26 +0900

    🚧 Change cli commands and add help

--- src/cli.rs
+++ src/cli.rs
@@ -10,32 +10,54 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
-    #[clap(about = "Versioning based on the current tag")]
-    Versioning(VersioningArgs),
-    #[clap(about = "Move to the last version")]
-    Teleport(TeleportArgs),
+    #[clap(about = "Related to next version actions")]
+    NextVersion(NextVersionArgs),
+    #[clap(about = "Related to last version actions")]
+    LastVersion(LastVersionArgs),
 }
 
 #[derive(Args, Debug)]
-pub struct VersioningArgs {
-    #[clap(long, action)]
+pub struct NextVersionArgs {
+    #[clap(help = "Get next major version", long, action)]
     pub major: bool,
-    #[clap(long, action)]
+    #[clap(help = "Get next minor version", long, action)]
     pub minor: bool,
-    #[clap(long, action)]
+    #[clap(help = "Get next patch version", long, action)]
     pub patch: bool,
-    #[clap(long)]
+    #[clap(help = "Get next version based on pattern", long)]
     pub pattern: Option<String>,
+    #[clap(help = "Tag current commit as next version", long, action)]
+    pub tag: bool,
+    #[clap(
+        help = "Tag current commit as next version and push to origin",
+        long,
+        action
+    )]
+    pub publish: bool,
 }
 
 #[derive(Args, Debug)]
-pub struct TeleportArgs {
-    #[clap(long, action)]
+pub struct LastVersionArgs {
+    #[clap(
+        help = "Get last version in the context of major version",
+        long,
+        action
+    )]
     pub major: bool,
-    #[clap(long, action)]
+    #[clap(
+        help = "Get last version in the context of minor version",
+        long,
+        action
+    )]
     pub minor: bool,
-    #[clap(long, action)]
+    #[clap(
+        help = "Get last version in the context of patch version",
+        long,
+        action
+    )]
     pub patch: bool,
-    #[clap(long)]
+    #[clap(help = "Get last version based on last version", long)]
     pub pattern: Option<String>,
+    #[clap(help = "Check out to last version", long, action)]
+    pub checkout: bool,
 }
